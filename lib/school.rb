8e6268388b571f3b5e0d53196cc3c9963d5eb1e1
Jordan Hayes <nosafoundation@gmail.com>
2020-03-17 23:51:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,43 +1,43 @@
 class School
   attr_accessor :name, :roster
-  
+
   def initialize(name)
     @name = name
     @roster = {}
-  end 
-  
+  end
+
   def add_student(student, level)
 #     # this sort of means #=> a || a = b ... or #=> x || x = y
 #     # it's freak'n weird
     roster[level] ||= []
     roster[level] << student
-  end 
+  end
   def grade(level)
-    roster.detect do |x, y| 
+    roster.detect do |x, y|
       if x == level
-        return y 
-      end 
-    end 
+        return y
+      end
+    end
 end 
-  
-# # This works, but it isn't passing learn.co's tests cause of 
+
+# # This works, but it isn't passing learn.co's tests cause of
 # # how it displays syntactically...but the same information is there
-# #  def sort 
-# #    roster.sort_by do |x, y| 
-# #      x 
-# #    end 
-# # #  end 
+# #  def sort
+# #    roster.sort_by do |x, y|
+# #      x
+# #    end
+# # #  end
 
 # # # Here's learn.co's solution:
-# # def sort 
+# # def sort
 # #   nu_hash = {}
-# #   roster.each do |x, y| 
-# #     nu_hash[x] = y.sort 
-# #   end 
+# #   roster.each do |x, y|
+# #     nu_hash[x] = y.sort
+# #   end
 # #   nu_hash
-# # end 
-  
-# # end 
+# # end
+
+# # end
 
 
 
@@ -66,21 +66,21 @@ end
 #       true
 #     else
 #       false
-      
+
 #     end
 
 #   end
- 
+
 #   def question?
 #     self.end_with?("?") ? true : false
 #   end
 
 #   def exclamation?
-#     self.end_with?("!") ? true : false 
+#     self.end_with?("!") ? true : false
 #   end
 
 #   def count_sentences
-    
+
 #     self.split(" ").length
 #   end
 # end
@@ -91,18 +91,18 @@ end
 
 # complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
 # #   # expect(complex_string.count_sentences).to eq(4)
-  
+
 # # #   complex_string.count_sentences
 # # # "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
 # complex_string.split(/[.?!]/)
 
 
-# class CashRegister 
-    
+# class CashRegister
+
 #     attr_reader :total, :emp_discount
 
 #     def initialize (emp_discount=0)
-       
+
 #         @total = 0
 #         @emp_discount = emp_discount
 #     end
@@ -111,24 +111,24 @@ end
 #         @total += price * quantity
 #     end
 
-# end 
+# end
 
 # cash_register = CashRegister.new(20)
 # cash_register.add_item("mango", 5)
 # cash_register.total
- 
- 
+
+
 class Song
- 
+
   @@all = []
- 
+
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
     @@all << self
   end
- 
+
   def self.all
     @@all.each do |song|
       puts song.name
@@ -140,5 +140,3 @@ sik_k = Song.new("rendevouz")
 uh = Song.new("huh")
 # sik_k.name
 Song.all
-
- 
